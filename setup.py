b9bc292e6992dde64d6f1a6edd878708239e2f94
FuouM <wr0n9.nuw133r@gmail.com>
2025-08-31 12:04:44 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+# ReEzSynth/setup.py
+import sys
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+extra_compile_args = {
+    "cxx": ["-O3", "-std=c++17"],
+    "nvcc": ["-O3", "-std=c++17"],
+}
+
+if sys.platform == "win32":
+    # MSVC specific flags
+    extra_compile_args["cxx"] = ["/O2", "/std:c++17", "/wd4244", "/wd4267"]
+    extra_compile_args["nvcc"] = ["-O3", "--use-local-env", "-std=c++17"]
+
+setup(
+    name="ebsynth_torch",
+    ext_modules=[
+        CUDAExtension(
+            name="ebsynth_torch",
+            sources=[
+                "ebsynth_extension/ext.cpp",
+                "ebsynth_extension/ebsynth.cu",
+            ],
+            extra_compile_args=extra_compile_args,
+        ),
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
